15b32447f0eec67381869a217b9cee9aaa4754ba
Zahra Vasanji <zahravasanji@hotmail.com>
2024-10-02 21:05:40 +0100

    modified 404 page

--- src/comps/NotFound.jsx
+++ src/comps/NotFound.jsx
@@ -1,14 +1,15 @@
 import { HashLink as Link } from "react-router-hash-link";
 import { Nav } from "./Nav";
-import "../styles/notFound.scss"
+import "../styles/notFound.scss";
 
 export const NotFound = () => {
   return (
     <div className="section notFound">
       <div className="page">
-  <h1>whoops...</h1>
-  <h2>that page can't be found</h2>
-
+        <div>
+        <h1>whoops...</h1>
+        <h2>that page can't be found</h2></div>
+        <video autoPlay muted loop src="/confused-john-travolta.mp4"/>
       </div>
     </div>
   );

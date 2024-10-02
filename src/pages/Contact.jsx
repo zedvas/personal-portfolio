86aff0be983935a8c9c5a7bffb328f173b3817e2
Zahra Vasanji <zahravasanji@hotmail.com>
2024-10-02 16:01:58 +0100

    added dynamic url change on scroll

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,20 +2,22 @@ import { Form } from "../comps/Form";
 import { HeroVideo } from "../comps/HeroVideo";
 import { JuggleContainer } from "../comps/JuggleContainer";
 import { Socials } from "../comps/Socials";
-import "../styles/contact.scss"
-
+import "../styles/contact.scss";
 
 export const Contact = ({ openReactModal, changeModalMessage }) => {
   return (
     <div className="contact section" id="contact">
       <div className="page">
-         
-
-
         <div className="juggleWrapper">
           <JuggleContainer />
         </div>
-        <Form openReactModal={ openReactModal} changeModalMessage={changeModalMessage }/>       <Socials/>
+        <div className="formContainer">
+          <Form
+            openReactModal={openReactModal}
+            changeModalMessage={changeModalMessage}
+          />
+          <Socials />
+        </div>
 
         {/* <HeroVideo/> */}
       </div>
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,16 +1,55 @@
 import { About } from "./About";
 import { Work } from "./Work";
 import { Contact } from "./Contact";
+import { useEffect, useRef } from "react";
 
-export const Pages = ({openReactModal, changeModalMessage}) => {
+export const Pages = ({ openReactModal, changeModalMessage }) => {
+  const about = useRef(null);
+  const work = useRef(null);
+  const contact = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = [about, work, contact];
+      const scrollPosition = window.scrollY;
+
+      sections.forEach((section) => {
+        const top = section.current.offsetTop;
+        const height = section.current.offsetHeight;
+        const bottom = top + height;
+
+        if (scrollPosition >= top && scrollPosition < bottom) {
+          window.history.replaceState(
+            null,
+            null,
+            `#${section.current.firstChild.id}`
+          );
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="pages">
-      <About />
-      <Work />
-      <Contact
-        openReactModal={openReactModal}
-        changeModalMessage={changeModalMessage}
-      />
+      <div ref={about}>
+        <About />
+      </div>
+      <div ref={work}>
+        <Work />
+      </div>
+      <div ref={contact}>
+        <Contact
+          ref={contact}
+          openReactModal={openReactModal}
+          changeModalMessage={changeModalMessage}
+        />
+      </div>
     </div>
   );
 };
